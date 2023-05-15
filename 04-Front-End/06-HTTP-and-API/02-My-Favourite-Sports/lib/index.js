8a94b757494fa4ec4e8413bc13d3795d777895d5
Marco Ricci <mr.ricci79@gmail.com>
2023-05-15 08:40:56 +0100

    geocoder completed

--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,18 @@
-// TODO: write your code here!
+const sports = document.querySelectorAll('.clickable');
+
+
+// sports.forEach((element) => {
+//   element.addEventListener('click', () => {
+//     element.classList.toggle('active');
+//   });
+// });
+
+const toggleClass = (e) => {
+  e.currentTarget.classList.toggle('active');
+};
+
+const bindElements = (element) => {
+  element.addEventListener('click', toggleClass);
+};
+
+sports.forEach(bindElements);
--- 04-Front-End/06-HTTP-and-API/03-Geocoder/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/03-Geocoder/lib/index.js
@@ -1,17 +1,44 @@
 // TODO: Call the Weather API when the form is submitted
+const form = document.querySelector('form');
+const token = "pk.eyJ1IjoibWFyY29yaXgiLCJhIjoiY2xhdGVkOGVrMG9sMTNzbXM4eHFpc29reiJ9.Yxophd6ZWayGqzN6M_OpNA";
+
+const displayCoordinates = (long, lat) => {
+  document.querySelector('p').innerText = `Longitude: ${long}, Latitude: ${lat}`;
+};
+
+const injectMap = (long, lat) => {
+  mapboxgl.accessToken = token;
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: [long, lat],
+    zoom: 12,
+  });
+
+  new mapboxgl.Marker().setLngLat([long, lat]).addTo(map);
+};
 
 // TODO: Create a function to get the weather info
-const getWeatherInfo = () => {
+const getWeatherInfo = (query) => {
   // TODO: Replace the following line with the correct url
-  const url = '...'
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${token}`;
 
   fetch(url)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
       // TODO: Insert the weather info in the DOM (description, date, temperature...)
+      const long = data.features[0].center[0];
+      const lat = data.features[0].center[1];
+      displayCoordinates(long, lat);
+      injectMap(long, lat);
     });
-}
+};
 
 // TODO: Add an event listener to the form
-// TODO: On submit, in the callback, call the getWeatherInfo function
+form.addEventListener('submit', (e) => {
+  // TODO: On submit, in the callback, call the getWeatherInfo function
+  e.preventDefault();
+  const query = document.querySelector("form > input").value;
+  getWeatherInfo(query);
+});
