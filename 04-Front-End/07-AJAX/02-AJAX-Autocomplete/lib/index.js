c9626922c641ad5e9b4c9ad88366a9d87da1adb2
Marco Ricci <mr.ricci79@gmail.com>
2023-05-16 08:38:10 +0100

    ajax autocomplete done

--- 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,16 @@
 // TODO: Autocomplete the input with AJAX calls.
+const ul = document.getElementById('results');
+const input = document.getElementById("search");
+
+const updateUl = (words) => {
+  ul.innerHTML = "";
+  words.forEach((word) => {
+    ul.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
+  });
+};
+
+input.addEventListener('keyup', () => {
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${input.value}`)
+    .then(res => res.json())
+    .then(data => updateUl(data.words));
+});
