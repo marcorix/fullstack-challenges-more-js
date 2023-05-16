a6750a39919113f144f8e3f7367416020174c94a
Marco Ricci <mr.ricci79@gmail.com>
2023-05-16 07:53:31 +0100

    agify api implemented

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,11 +1,16 @@
-const agifyApiUrl = "https://api.agify.io/"
+const agifyApiUrl = "https://api.agify.io/";
 
 const displayAge = (event) => {
   event.preventDefault();
   const firstName = document.getElementById("first-name").value;
   // TODO: Display your age with an AJAX call instead of the console.log()
+  fetch(`${agifyApiUrl}?name=${firstName}`)
+    .then(res => res.json())
+    .then((data) => {
+      document.getElementById("your-age").innerText = `You are ${data.age} years old`;
+    });
   console.log(firstName);
-}
+};
 
 const form = document.getElementById("fetch-age");
 form.addEventListener("submit", displayAge);
